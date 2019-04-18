a76c7a725ed06091e6af22a5d39e03ae7654025c
Mark Lee <electronjs@lazymalevolence.com>
2019-04-17 21:50:40 -0700

    refactor: finish promisify conversion & use async/await everywhere

--- test/api-spec.js
+++ test/api-spec.js
@@ -1,12 +1,10 @@
 'use strict'
 
-const { promisify } = require('util')
-
 const assert = require('assert')
-const fs = promisify(process.versions.electron ? require('original-fs') : require('fs'))
+const fs = require('../lib/wrapped-fs')
 const os = require('os')
 const path = require('path')
-const rimraf = promisify(require('rimraf'))
+const rimraf = require('rimraf')
 
 const asar = require('..')
 const compDirs = require('./util/compareDirectories')
@@ -14,82 +12,82 @@ const compFileLists = require('./util/compareFileLists')
 const compFiles = require('./util/compareFiles')
 const transform = require('./util/transformStream')
 
-function assertPackageListEquals (actualList, expectedFilename) {
-  return fs.readFile(expectedFilename, 'utf8')
-    .then(expected => compFileLists(actualList.join('\n'), expected))
+async function assertPackageListEquals (actualList, expectedFilename) {
+  const expected = await fs.readFile(expectedFilename, 'utf8')
+  return compFileLists(actualList.join('\n'), expected)
 }
 
 describe('api', function () {
   beforeEach(() => { rimraf.sync(path.join(__dirname, '..', 'tmp'), fs) })
 
-  it('should create archive from directory', () => {
-    return asar.createPackage('test/input/packthis/', 'tmp/packthis-api.asar')
-      .then(() => compFiles('tmp/packthis-api.asar', 'test/expected/packthis.asar'))
+  it('should create archive from directory', async () => {
+    await asar.createPackage('test/input/packthis/', 'tmp/packthis-api.asar')
+    return compFiles('tmp/packthis-api.asar', 'test/expected/packthis.asar')
   })
   if (os.platform() === 'win32') {
-    it('should create archive with windows-style path separators', () => {
-      asar.createPackage('test\\input\\packthis\\', 'tmp\\packthis-api.asar')
-        .then(() => compFiles('tmp/packthis-api.asar', 'test/expected/packthis.asar'))
+    it('should create archive with windows-style path separators', async () => {
+      await asar.createPackage('test\\input\\packthis\\', 'tmp\\packthis-api.asar')
+      return compFiles('tmp/packthis-api.asar', 'test/expected/packthis.asar')
     })
   }
-  it('should create archive from directory (without hidden files)', () => {
-    return asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-without-hidden-api.asar', { dot: false })
-      .then(() => compFiles('tmp/packthis-without-hidden-api.asar', 'test/expected/packthis-without-hidden.asar'))
+  it('should create archive from directory (without hidden files)', async () => {
+    await asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-without-hidden-api.asar', { dot: false })
+    return compFiles('tmp/packthis-without-hidden-api.asar', 'test/expected/packthis-without-hidden.asar')
   })
-  it('should create archive from directory (with transformed files)', () => {
-    return asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-api-transformed.asar', { transform })
-      .then(() => compFiles('tmp/packthis-api-transformed.asar', 'test/expected/packthis-transformed.asar'))
+  it('should create archive from directory (with transformed files)', async () => {
+    await asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-api-transformed.asar', { transform })
+    return compFiles('tmp/packthis-api-transformed.asar', 'test/expected/packthis-transformed.asar')
   })
-  it('should create archive from directory (with nothing packed)', () => {
-    return asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-api-unpacked.asar', { unpackDir: '**' })
-      .then(() => compFiles('tmp/packthis-api-unpacked.asar', 'test/expected/packthis-all-unpacked.asar'))
-      .then(() => compDirs('tmp/packthis-api-unpacked.asar.unpacked', 'test/expected/extractthis'))
+  it('should create archive from directory (with nothing packed)', async () => {
+    await asar.createPackageWithOptions('test/input/packthis/', 'tmp/packthis-api-unpacked.asar', { unpackDir: '**' })
+    await compFiles('tmp/packthis-api-unpacked.asar', 'test/expected/packthis-all-unpacked.asar')
+    return compDirs('tmp/packthis-api-unpacked.asar.unpacked', 'test/expected/extractthis')
   })
-  it('should list files/dirs in archive', function () {
+  it('should list files/dirs in archive', async () => {
     return assertPackageListEquals(asar.listPackage('test/input/extractthis.asar'), 'test/expected/extractthis-filelist.txt')
   })
-  it('should list files/dirs in archive with option', function () {
+  it('should list files/dirs in archive with option', async () => {
     return assertPackageListEquals(asar.listPackage('test/input/extractthis-unpack-dir.asar', { isPack: true }), 'test/expected/extractthis-filelist-with-option.txt')
   })
-  it('should extract a text file from archive', function () {
+  it('should extract a text file from archive', async () => {
     const actual = asar.extractFile('test/input/extractthis.asar', 'dir1/file1.txt').toString('utf8')
-    let expected = fs.readFileSync('test/expected/extractthis/dir1/file1.txt', 'utf8')
+    let expected = await fs.readFile('test/expected/extractthis/dir1/file1.txt', 'utf8')
     return compFileLists(actual, expected)
   })
-  it('should extract a binary file from archive', function () {
+  it('should extract a binary file from archive', async () => {
     const actual = asar.extractFile('test/input/extractthis.asar', 'dir2/file2.png')
-    const expected = fs.readFileSync('test/expected/extractthis/dir2/file2.png')
+    const expected = await fs.readFile('test/expected/extractthis/dir2/file2.png')
     return assert.strictEqual(actual.toString(), expected.toString())
   })
-  it('should extract a binary file from archive with unpacked files', function () {
+  it('should extract a binary file from archive with unpacked files', async () => {
     const actual = asar.extractFile('test/input/extractthis-unpack.asar', 'dir2/file2.png')
-    const expected = fs.readFileSync('test/expected/extractthis/dir2/file2.png')
+    const expected = await fs.readFile('test/expected/extractthis/dir2/file2.png')
     return assert.strictEqual(actual.toString(), expected.toString())
   })
-  it('should extract an archive', () => {
+  it('should extract an archive', async () => {
     asar.extractAll('test/input/extractthis.asar', 'tmp/extractthis-api/')
     return compDirs('tmp/extractthis-api/', 'test/expected/extractthis')
   })
-  it('should extract an archive with unpacked files', () => {
+  it('should extract an archive with unpacked files', async () => {
     asar.extractAll('test/input/extractthis-unpack.asar', 'tmp/extractthis-unpack-api/')
     return compDirs('tmp/extractthis-unpack-api/', 'test/expected/extractthis')
   })
-  it('should extract a binary file from archive with unpacked files', function () {
+  it('should extract a binary file from archive with unpacked files', async () => {
     const actual = asar.extractFile('test/input/extractthis-unpack-dir.asar', 'dir1/file1.txt')
-    const expected = fs.readFileSync('test/expected/extractthis/dir1/file1.txt')
-    return assert.strictEqual(actual.toString(), expected.toString())
+    const expected = await fs.readFile('test/expected/extractthis/dir1/file1.txt')
+    assert.strictEqual(actual.toString(), expected.toString())
   })
-  it('should extract an archive with unpacked dirs', () => {
+  it('should extract an archive with unpacked dirs', async () => {
     asar.extractAll('test/input/extractthis-unpack-dir.asar', 'tmp/extractthis-unpack-dir-api/')
     return compDirs('tmp/extractthis-unpack-dir-api/', 'test/expected/extractthis')
   })
-  it('should handle multibyte characters in paths', () => {
-    return asar.createPackage('test/input/packthis-unicode-path/', 'tmp/packthis-unicode-path.asar')
-      .then(() => compFiles('tmp/packthis-unicode-path.asar', 'test/expected/packthis-unicode-path.asar'))
+  it('should handle multibyte characters in paths', async () => {
+    await asar.createPackage('test/input/packthis-unicode-path/', 'tmp/packthis-unicode-path.asar')
+    return compFiles('tmp/packthis-unicode-path.asar', 'test/expected/packthis-unicode-path.asar')
   })
-  it('should extract a text file from archive with multibyte characters in path', function () {
+  it('should extract a text file from archive with multibyte characters in path', async () => {
     const actual = asar.extractFile('test/expected/packthis-unicode-path.asar', 'dir1/女の子.txt').toString('utf8')
-    let expected = fs.readFileSync('test/input/packthis-unicode-path/dir1/女の子.txt', 'utf8')
+    let expected = await fs.readFile('test/input/packthis-unicode-path/dir1/女の子.txt', 'utf8')
     return compFileLists(actual, expected)
   })
 })
