eeaa9107c14d2be5490266c6482c996c13fa95f5
Samuel Attard <samuel.r.attard@gmail.com>
2021-08-31 17:19:02 -0700

    feat: add block hashes

--- test/util/compareFiles.js
+++ test/util/compareFiles.js
@@ -4,6 +4,9 @@ const assert = require('assert')
 const fs = require('../../lib/wrapped-fs')
 
 module.exports = async function (actualFilePath, expectedFilePath) {
+  if (process.env.ELECTRON_ASAR_SPEC_UPDATE) {
+    await fs.writeFile(expectedFilePath, await fs.readFile(actualFilePath))
+  }
   const [actual, expected] = await Promise.all([fs.readFile(actualFilePath, 'utf8'), fs.readFile(expectedFilePath, 'utf8')])
   assert.strictEqual(actual, expected)
 }
