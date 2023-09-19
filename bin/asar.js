f60a4c11641a401689a0fa55f67197c82ad491f3
Erik Moura <erikian@erikian.dev>
2023-09-18 21:25:17 -0300

    fix: properly handle rejections on `createPackageWithOptions` (#254)
    fix: properly handle rejections on createPackageWithOptions

--- bin/asar.js
+++ bin/asar.js
@@ -35,11 +35,9 @@ program.command('pack <dir> <output>')
       builddir: options.sb,
       dot: !options.excludeHidden
     }
-    asar.createPackageWithOptions(dir, output, options, function (error) {
-      if (error) {
-        console.error(error.stack)
-        process.exit(1)
-      }
+    asar.createPackageWithOptions(dir, output, options).catch(error => {
+      console.error(error)
+      process.exit(1)
     })
   })
 
