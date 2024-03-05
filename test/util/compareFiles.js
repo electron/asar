a9b4e7b8722a2ec3e7520cd2ab2eea0c1b09d109
JrainLau <jrainlau@gmail.com>
2024-03-06 07:47:13 +0800

    fix: fix symlink pointing error in filesystem.insertLink (#301)
    Co-authored-by: jrainliu <jrainliu@tencent.com>

--- test/util/compareFiles.js
+++ test/util/compareFiles.js
@@ -7,6 +7,19 @@ module.exports = async function (actualFilePath, expectedFilePath) {
   if (process.env.ELECTRON_ASAR_SPEC_UPDATE) {
     await fs.writeFile(expectedFilePath, await fs.readFile(actualFilePath))
   }
-  const [actual, expected] = await Promise.all([fs.readFile(actualFilePath, 'utf8'), fs.readFile(expectedFilePath, 'utf8')])
-  assert.strictEqual(actual, expected)
+  const [actualFileContent, expectedFileContent] = await Promise.all([fs.readFile(actualFilePath, 'utf8'), fs.readFile(expectedFilePath, 'utf8')])
+  assert.strictEqual(actualFileContent, expectedFileContent)
+
+  const [actualIsSymlink, expectedIsSymlink] = [isSymbolicLinkSync(actualFilePath), isSymbolicLinkSync(expectedFilePath)]
+  assert.strictEqual(actualIsSymlink, expectedIsSymlink)
+
+  if (actualIsSymlink && expectedIsSymlink) {
+    const [actualSymlinkPointer, expectedSymlinkPointer] = [fs.readlinkSync(actualFilePath), fs.readlinkSync(expectedFilePath)]
+    assert.strictEqual(actualSymlinkPointer, expectedSymlinkPointer)
+  }
+}
+
+function isSymbolicLinkSync (path) {
+  const stats = fs.lstatSync(path)
+  return stats.isSymbolicLink()
 }
