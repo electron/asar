880d020d7aab91742aa55fdcb5e975dfd06fc446
Kevin Cui <bh@bugs.cc>
2024-03-28 19:14:26 +0800

    fix test
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- test/filesystem-spec.js
+++ test/filesystem-spec.js
@@ -1,16 +1,42 @@
 'use strict'
 
 const assert = require('assert')
+const fs = require('../lib/wrapped-fs')
 const path = require('path')
+const rimraf = require('rimraf')
 
 const Filesystem = require('../lib/filesystem')
 
 describe('filesystem', function () {
+  beforeEach(() => { rimraf.sync(path.join(__dirname, '..', 'tmp'), fs) })
+
   it('should does not throw error when src path include symbol link', async () => {
-    const src = path.join(__dirname, 'input', 'srcpath-include-symlink', 'var', 'app')
-    const filesystem = new Filesystem(src)
+    /* eslint-disable no-irregular-whitespace */
+    /**
+     * Directory structure:
+     * tmp
+     * ├── private
+     * │   └── var
+     * │       ├── app
+     * │       │   └── file.txt -> ../file.txt
+     * │       └── file.txt
+     * └── var -> private/var
+     */
+    const tmpPath = path.join(__dirname, '..', 'tmp')
+    const privateVarPath = path.join(tmpPath, 'private', 'var')
+    const varPath = path.join(tmpPath, 'var')
+    fs.mkdirSync(privateVarPath, { recursive: true })
+    fs.symlinkSync(path.relative(tmpPath, privateVarPath), varPath)
+
+    const originFilePath = path.join(varPath, 'file.txt')
+    fs.writeFileSync(originFilePath, 'hello world')
+    const appPath = path.join(varPath, 'app')
+    fs.mkdirpSync(appPath)
+    fs.symlinkSync('../file.txt', path.join(appPath, 'file.txt'))
+
+    const filesystem = new Filesystem(varPath)
     assert.doesNotThrow(() => {
-      filesystem.insertLink(path.join(src, 'symbol', 'real.txt'))
+      filesystem.insertLink(path.join(appPath, 'file.txt'))
     })
   })
 })
