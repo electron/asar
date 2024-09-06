7048cd0b417c5597aaa4c508f14f308053dc4a16
Samuel Attard <samuel.r.attard@gmail.com>
2024-09-06 16:08:34 -0700

    chore: remove unused imports

--- src/asar.ts
+++ src/asar.ts
@@ -4,9 +4,8 @@ import * as minimatch from 'minimatch';
 import fs from './wrapped-fs';
 import { Filesystem, FilesystemEntry } from './filesystem';
 import * as disk from './disk';
-import { CrawledFileType, crawl as crawlFilesystem, determineFileType } from './crawlfs';
+import { crawl as crawlFilesystem, determineFileType } from './crawlfs';
 import { IOptions } from 'glob';
-import { Stats } from 'fs';
 
 /**
  * Whether a directory should be excluded from packing due to the `--unpack-dir" option.
