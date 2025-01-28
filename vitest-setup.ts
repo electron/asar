9d4ab0927f04142dc581ac853a1aca8b077054fb
Mike Maietta <mmaietta@outlook.com>
2025-01-27 22:11:03 -0800

    cleanup tmp dir

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -2,4 +2,4 @@ import * as path from 'path';
 import rimraf from 'rimraf';
 import fs from './src/wrapped-fs';
 
-rimraf.sync(path.join(__dirname, '..', 'tmp'), fs);
+rimraf.sync(path.join(__dirname, 'tmp'), fs);
